cdfed0521b60d570b55bd0092c62dc70c9d16922
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-03-13 18:36:26 -0700

    added test route

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+if(process.env.NODE_ENV === 'development') {
+  require("dotenv").config();
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -19,12 +24,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-if(process.env.NODE_ENV === 'development') {
-  require("dotenv").config();
-}
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
