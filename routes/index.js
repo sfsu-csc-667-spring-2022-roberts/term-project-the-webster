ba103a07411989324e1d00a1ebd11ff900eb3f71
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-03-13 20:35:52 -0700

    fixed grammar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team Webster Scrabble' });
+  res.render('index', { title: 'Team Websters Scrabble' });
 });
 
 module.exports = router;
