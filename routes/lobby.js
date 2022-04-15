48d87382118fa92c082fa90cdbaf2ce8539db222
jackderemiah <jderemiah@mail.sfsu.edu>
2022-04-15 16:16:18 -0700

    added partials to lobby page

--- routes/lobby.js
+++ routes/lobby.js
@@ -3,8 +3,29 @@ const router = express.Router();
 const db = require('../db');
 
 router.get("/", (request, response) => {
-    response.render('lobby', {title: 'lobby'});
+  
+    response.render('lobby', {
+        title: 'lobby',
+        messages: [
+          {
+            id: 1,
+            timestamp: " 21:03",
+            content: 'hello',
+          },
+          {
+            id: 2,
+            timestamp: ` 21:05 `,
+            content: "hey",
+          },
+          {
+            id: 3,
+            timestamp: `21:05 `,
+            content: "yo",
+          }
+        ]
+      });
    
-});
+    });
 
 module.exports = router;
+
