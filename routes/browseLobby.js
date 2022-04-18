76afd9f3042d3970b69f3cb77224d0ac14a1aef5
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2022-04-18 13:48:01 -0700

    fix duplicate css issue

--- routes/browseLobby.js
+++ routes/browseLobby.js
@@ -5,28 +5,7 @@ const db = require('../db');
 router.get("/", (request, response) => {
     response.render('browseLobby', {
         title: 'browse',
-        lobbies: [
-            {
-                id: 1,
-                name: "Test Lobby",
-                host: "Bob Jones",
-                players: 4
-            },
-            {
-                id: 2,
-                name: "Come play Scrabble",
-                host: "Damon",
-                players: 2
-            },
-            {
-                id: 3,
-                name: "Fun times",
-                host: "Johnson",
-                players: 3
-            }
-        ]
     });
 });
-
 
 module.exports = router;
