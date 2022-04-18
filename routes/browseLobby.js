cd5c3b1ebdf7e8190822c78ff211cb867bc9e061
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2022-04-18 12:22:13 -0700

    add dummy data for lobbies

--- routes/browseLobby.js
+++ routes/browseLobby.js
@@ -3,9 +3,29 @@ const router = express.Router();
 const db = require('../db');
 
 router.get("/", (request, response) => {
-    response.render('browseLobby', { title: 'browse  ' , 
-    block: '' });
-    // response.send("lobby page!")   
+    response.render('browseLobby', {
+        title: 'browse',
+        lobbies: [
+            {
+                id: 1,
+                name: "Test Lobby",
+                host: "Bob Jones",
+                players: 4
+            },
+            {
+                id: 2,
+                name: "Come play Scrabble",
+                host: "Damon",
+                players: 2
+            },
+            {
+                id: 3,
+                name: "Fun times",
+                host: "Johnson",
+                players: 3
+            }
+        ]
+    });
 });
 
 
