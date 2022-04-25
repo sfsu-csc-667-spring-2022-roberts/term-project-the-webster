9e8bc66f5b7a5d596b73017c79cf048ea60b6f3a
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-04-24 23:26:20 -0700

    made it so it wont insert into db on register if user already exists

--- models/Users.js
+++ models/Users.js
@@ -2,22 +2,6 @@ const db = require('../db');
 const bcrypt = require('bcrypt');
 const UserModel = {};
 
-// UserModel.create = (username, password) => 
-// {
-//     bcrypt.hash(password, 15)
-//     .then( (hashedPassword) => 
-//     {
-//         const CREATE_USER_QUERY = 
-//         'INSERT INTO users ("username", "password") VALUES ($1,$2)';
-//         return db.any(CREATE_USER_QUERY,[username, hashedPassword ]);
-//     })
-//     .then(results => {
-//         console.log(results);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
-// }
 
 //TO DO make this return results of query
 UserModel.create = (username, password ) => 
@@ -26,50 +10,68 @@ UserModel.create = (username, password ) =>
     .then( (hashedPassword) =>
     {
         let baseSQL = 'INSERT INTO users ("username", "password") VALUES ($1,$2)';
-        db.any(baseSQL, [username, hashedPassword]);
+        return db.any(baseSQL, [username, hashedPassword]);
     })
+    .then(results => {
+        console.log("results from create is:" + results);
+        if(results && results.affectedRows)
+        {
+            console.log("insertID is:" + results.insertId);
+            return Promise.resolve(results.insertId);
+        }
+        else 
+        {
+            console.log("else statement");
+            return Promise.resolve(-1);
+        }
+    })
+    // .catch( (err) => console.log("create error is:" + err));
     .catch( (err) => Promise.reject(err));
 }
 
-// UserModel.usernameExists = (username) =>
-// {
-//     return db.any("SELECT * FROM users WHERE username=?", [username])
-//     .then(([results, fields]) => {
-//         return Promise.resolve(!(results && results.length == 0));
-//     }) 
-//     .catch( (err) => Promise.reject(err));
-// }
+UserModel.usernameExists = (username) =>
+{
+    return db.any("SELECT * FROM users WHERE username=$1", [username])
+    .then((results) => {
+        return Promise.resolve(!(results && results.length == 0));
+    }) 
+    .catch( (err) => Promise.reject(err));
+}
 
-// UserModel.authenticate = (username, password) => 
-// {
-//     let userId; 
-//     let baseSQL = "SELECT id, username, password FROM users WHERE username=?;";
-//     return db.any(baseSQL, [username])
-//     .then( ([results, fields]) =>
-//     {
-//         if (results && results.length == 1)
-//         {
-//             userId = results[0].id;
-//             return bcrypt.compare(password, results[0].password);
-//         }
-//         else 
-//         {
-//             return Promise.resolve(-1);
-//         }
-//     })
-//     .then( (passwordsMatch) => 
-//     {
-//         if(passwordsMatch)
-//         {
-//             return Promise.resolve(userId);
-//         }
-//         else 
-//         {
-//             return Promise.resolve(-1);
-//         }
-//     })
-//     .catch( (err) => Promise.reject(err));
-// };
+UserModel.authenticate = (username, password) => 
+{
+    let userId; 
+    let baseSQL = `SELECT "userID", username, password FROM users WHERE username=$1;`;
+    return db.any(baseSQL, [username])
+    .then( ([results, fields]) =>
+    {
+        console.log(results);
+        if (results && results.length == 1)
+        {
+            userId = results[0].id;
+            return bcrypt.compare(password, results[0].password);
+        }
+        else 
+        {
+            console.log("cannot find user");
+            return Promise.resolve(-1);
+        }
+    })
+    .then( (passwordsMatch) => 
+    {
+        if(passwordsMatch)
+        {
+            console.log("password matches");
+            return Promise.resolve(userId);
+        }
+        else 
+        {
+            console.log("password does not match");
+            return Promise.resolve(-1);
+        }
+    })
+    .catch( (err) => Promise.reject(err));
+};
 
 
 module.exports = UserModel;
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const db = require('../db');
 var router = express.Router();
 
 const UserModel = require('../models/Users');
+const e = require('express');
 
 
 //TODO verify if username already in DB
@@ -17,18 +18,43 @@ router.post("/register", async (req, res, next)=> {
     //TODO make this not insert to DB if it does not match
     console.log("passwords do not match");
   }
-  console.log("HERE");
+  console.log("REGISTER IS RUNNING");
 
-  UserModel.create(username, password)
+  UserModel.usernameExists(username)
+  .then( (userDoesExist) => {
+    if(userDoesExist) {
+      console.log("USER EXISTS");
+    }
+    else {
+      console.log("USER DOES NOT EXISTSS");
+      return UserModel.create(username, password);
+    }
+  })
+  .then((createUserId) => {
+    //TODO make this get from the create query
+    console.log("AHHHH");
+    res.redirect('/login')
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+});
+
+router.post("/login", async (req, res, next)=> {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log("LOGIN IS RUNNING");
+  console.log(username);
+  console.log(password);
+
+  UserModel.authenticate(username, password)
   .then(results => {
-    res.redirect('/login');
+    res.redirect('/lobby');
   })
   .catch(err => {
     console.log(err);
     next(err);
   })
-
-
 });
 
 module.exports = router;
