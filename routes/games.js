b8a5f64305eaa39726b3b5d190f66e8d01c805e7
krisbyington <43834863+krisbyington@users.noreply.github.com>
2022-04-26 11:47:54 -0700

    Create games.js
    adding games route for easy merge and refactor

---
+++ routes/games.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const router = express.Router();
+const Game = require("../db/game");
+
+router.get("/create", (request, response) => {
+  const currentUser = 1; // don't hard code this, get from params
+
+  Game.createGame(currentUser)
+    .then(({ gameId }) => {
+      response.redirect(`/game/${gameId}`);
+    })
+    .catch((error) => {
+      console.log(error);
+      response.redirect("/lobby");
+    });
+});
+
+router.get("/:id", (request, response) => {
+  Game.getEmptyGrid()
+    .then((cells) => {
+      response.render("games", { cells });
+    })
+    .catch((error) => {
+      console.log(">", error);
+      response.json({ error });
+    });
+});
+
+router.get("/:id/join", (request, response) => {
+  const { id: gameId } = request.params;
+  const userId = 1; // This should be based on the current logged in user
+
+  Game.joinGame(gameId, userId)
+    .then(({ gameId }) => {
+      response.redirect(`/game/${gameId}`);
+      // Broadcast to game socket that a user has joind the game
+    })
+    .catch((error) => {
+      console.log(error);
+      response.redirect("/lobby");
+    });
+});
+
+module.exports = router;
