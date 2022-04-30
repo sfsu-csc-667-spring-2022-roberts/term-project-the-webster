55e744f590b2019bb1de5d2507b40fee5f63a873
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-04-29 21:24:04 -0700

    removed curly brackets in createGame in db/game.js

--- db/game.js
+++ db/game.js
@@ -5,11 +5,11 @@ const getEmptyGrid = () => db.any("SELECT * FROM game_grid ORDER BY x, y ASC");
 const createGame = (userId) =>
   db
     .one('INSERT INTO games ("in_lobby") VALUES (true) RETURNING id AS "game_id"')
-    .then(( {game_id} ) => {
+    .then(( {game_id} ) =>
       db.one(
         'INSERT INTO game_users ("game_id", "user_id", "order") VALUES ($1, $2, 0) RETURNING "game_id"',
         [game_id, userId]
-      )}
+      )
     );
     // .then(({game_id}) => 
     //   db.any("SELECT * FROM tiles").then((tiles) => {
