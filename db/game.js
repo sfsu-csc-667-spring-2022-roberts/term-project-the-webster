a374e64cb408422173f1382933e47fa2c16fe392
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2022-05-03 21:26:13 -0700

    start work on tiles bulk insert

--- db/game.js
+++ db/game.js
@@ -2,23 +2,39 @@ const db = require("./index");
 
 const getEmptyGrid = () => db.any("SELECT * FROM game_grid ORDER BY x, y ASC");
 
-const createGame = (userId) =>
-  db
-    .one('INSERT INTO games ("in_lobby") VALUES (true) RETURNING id AS "game_id"')
-    .then(( {game_id} ) =>
-      db.one(
-        'INSERT INTO game_users ("game_id", "user_id", "order") VALUES ($1, $2, 0) RETURNING game_id',
-        [game_id, userId]
-      )
-    );
-    // .then(({game_id}) => 
-    //   db.any("SELECT * FROM tiles").then((tiles) => {
-    //     // INSERT tiles INTO game_tiles (with default values) RETURNING "gameId" in random order
-    //     data = db.one('INSERT INTO game_tiles ("game_id", "user_id", "tile_id", "order") VALUES (1,1,1, 0) RETURNING "game_id"');
-    //     return data;
-    //   })
-    // );
+const insertGames = (userId) => {
+  return db.one('INSERT INTO games ("in_lobby") VALUES (true) RETURNING id AS "game_id"');
+}
 
+async function createGame(userId) {
+  console.log("greetings everyone");
+  game_id = await insertGames(userId);
+  gameId = game_id.game_id;
+  console.log(gameId);
+  console.log("user_id: " + userId);
+  console.log("test");
+  db.any('SELECT * FROM tiles').then((tiles) => {
+    console.log("tiles");
+    console.log(tiles);
+    db.one('INSERT INTO game_users ("game_id", "user_id", "order") VALUES ($1, $2, 0)', [gameId, userId])
+    .then(() => {
+      db.one('INSERT INTO game_tiles ("game_id", "user_id", "tile_id", "order") VALUES (1, 1, 1, 0)').then(() => {
+        return game_id;
+      })
+    })
+  })
+  return game_id;
+       /*db.any("SELECT * FROM tiles").then((tiles) => {
+         // INSERT tiles INTO game_tiles (with default values) RETURNING "gameId" in random order
+         data = db.one(
+           'INSERT INTO game_tiles ("game_id", "user_id", "tile_id", "order") VALUES (1,1,1,0) RETURNING "game_id"',
+         [game_id, tiles]
+           );
+           console.log("hey what's up");
+         return data; 
+       })
+      });*/
+}
 
 const joinGame = (gameId, userId) =>
   db.one(
