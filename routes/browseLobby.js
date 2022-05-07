8f91331a0be6e27032ff3dc2c15357e12a9cf2ab
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2022-05-07 12:41:28 -0700

    redesign broseLobbies

--- routes/browseLobby.js
+++ routes/browseLobby.js
@@ -2,10 +2,29 @@ const express = require("express");
 const router = express.Router();
 const db = require('../db');
 
+//temp import, will need to be encapsulated into models/gameboard
+const Game = require("../db/game");
+
 router.get("/", (request, response) => {
-    response.render('browseLobby', {
+    console.log("games");
+    Game.getGames().then((games) => {
+        console.log(games);
+        response.render('browseLobby', {
+            style: 'style',
+            lobbies: games
+        });
+    })
+    /*.then((games) => {
+        console.log(games);
+        response.render('browseLobby', {
+            style: 'style',
+            lobbies: games
+        });
+    });*/
+    /*response.render('browseLobby', {
         style: 'style',
-    });
+        // lobbies: games
+    });*/
 });
 
 module.exports = router;
